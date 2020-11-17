6dc7a2a780229c11e1395465bf169e7dfb42a011
Ryan Witherspoon <ry.witherspoon@gmail.com>
2020-11-16 20:23:59 -0700

    Updated server file

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
-const express = require("express");
-const mongoose = require("mongoose");
+const express = require("express"); // npm express
+const logger = require("morgan"); // npm logger
+const mongoose = require("mongoose"); // npm mongoose
 
 const PORT = process.env.PORT || 3000;
 
 const db = require("./models");
 
 const app = express();
+
+app.use(logger("dev"));
+app.use(express.urlencoded ({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitnessWorkout", { useNewUrlParser: true});
+
+// establish connection to server
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`)
+})
